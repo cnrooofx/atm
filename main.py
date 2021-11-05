2effe5815454792ec1a75009b6a0e9f946a8104f
Conor Fox <conor@conorfox.com>
2021-11-05 19:10:35 +0000

    Update to use rich library for terminal printing

--- main.py
+++ main.py
@@ -1,27 +1,62 @@
-import os
-from platform import system
+"""Install requirements with `pip3 install -r requirements.txt`"""
+
+from rich.console import Console
+from rich.panel import Panel
+
+from atm import ATM
+
+console = Console()
+atm = ATM()
 
 
 def start_atm():
-    clear_screen()
+    menu_selection = None
+    while menu_selection is None:
+        console.clear()
+        console.print(Panel.fit("Welcome"))
+        console.print("1) Customer")
+        console.print("2) Admin")
+        console.print("\nPlease select an option.")
 
-    print("Welcome")
-    print("-------")
-    print("1) Customer")
-    print("2) Admin")
-
-    print("\nPlease select an option.")
-    try:
-        selection = get_user_selection(["1", "2"])
-    except ValueError:
-        # Return to start
-        return
-    if selection == "2":
-        clear_screen()
-        print("Admin Menu")
+        menu_selection = get_user_selection(["1", "2"])
+            
+    if menu_selection == "2":
+        admin_menu()
     else:
-        clear_screen()
-        print("Main Menu")
+        # Maybe a user login before going to the menu?
+        main_menu()
+
+
+def admin_menu():
+    console.clear()
+    console.print(Panel.fit("Admin Menu"))
+
+
+def main_menu():
+    menu_selection = None
+    while menu_selection is None:
+        console.clear()
+        console.print(Panel.fit("Main Menu"))
+        console.print("1) Check Balance")
+        console.print("2) Withdraw")
+        console.print("3) Deposit")
+        console.print("4) Transfer Funds")
+        console.print("5) Reset PIN")
+        console.print("\nPlease select an option or press (l) to Logout.")
+
+        menu_selection = get_user_selection(["1", "2", "3", "4", "5", "l"])
+
+    if menu_selection == "1":
+        print("Check balance")
+    elif menu_selection == "2":
+        print("Withdraw")
+    elif menu_selection == "3":
+        print("Deposit")
+    elif menu_selection == "4":
+        print("Transfer")
+    elif menu_selection == "5":
+        print("Reset PIN")
+
 
 def get_user_selection(options):
     """Get user input and check against a list of valid options.
@@ -29,32 +64,13 @@ def get_user_selection(options):
     Args:
         options (list): List of valid input options.
 
-    Raises:
-        ValueError: If the input doesn't match any of the options.
-
     Returns:
-        user_input (str): One of the valid options from the user.
+        user_input (str): One of the valid options from the user or None.
     """
     user_input = input("-> ")
     if user_input not in options:
-        raise ValueError("Invalid option")
+        user_input = None
     return user_input
-
-
-
-def clear_screen():
-    """Clear the terminal screen on Windows, Linux and Mac.
-
-    Raises:
-        RuntimeError: If system type is not supported.
-    """
-    system_type = system()
-    if system_type == "Windows":
-        os.system("cls")
-    elif system_type in ["Linux", "Darwin"]:
-        os.system("clear")
-    else:
-        raise RuntimeError("Incompatible system")
 
 
 if __name__ == "__main__":
