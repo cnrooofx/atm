8aef20c1c2b42f29559503f72debff18542ab851
Conor Fox <conor@conorfox.com>
2021-11-10 19:42:09 +0000

    Exceptions

---
+++ exceptions.py
@@ -0,0 +1,39 @@
+"""Custom Exceptions for the ATM."""
+
+class AccountError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return f"AccountError: {self.message}"
+        return "AccountError"
+
+
+class AtmError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return f"AtmError: {self.message}"
+        return "AtmError"
+
+
+class BankError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return f"BankError: {self.message}"
+        return "BankError"
